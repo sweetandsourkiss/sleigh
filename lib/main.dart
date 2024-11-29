9d10ef14ebedb715ea64cdce2dacd6e4d8fdf104
Jay Son <mooul0529@naver.com>
2024-11-29 17:20:27 +0900

    Add Flame Game widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:spring/spring_game.dart';
 
 void main() {
-  final game = FlameGame();
+  final game = SpringGame();
   runApp(
     GameWidget(
       game: game,
---
+++ lib/spring_game.dart
@@ -0,0 +1,3 @@
+import 'package:flame/game.dart';
+
+class SpringGame extends FlameGame {}

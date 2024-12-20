88939dde83241f37e1c02463dda93e9b1a49afb9
Jay Son <mooul0529@naver.com>
2024-12-20 17:00:20 +0900

    Learn camera viewfinder

--- lib/spring.dart
+++ lib/spring.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
 import 'package:spring/components/test.dart';
 
 class Spring extends FlameGame {
@@ -11,24 +10,18 @@ class Spring extends FlameGame {
       ..size = Vector2(size.x, size.y)
       ..position = Vector2(0, 0);
     world.add(testComponent);
-    world.debugColor = Colors.amber;
+    camera.viewfinder.anchor = Anchor.topCenter;
     camera.viewfinder.visibleGameSize = Vector2(size.x, size.y);
     camera.viewfinder.position = Vector2(size.x / 2, 0);
-    camera.viewfinder.anchor = Anchor.topCenter;
   }
 
   @override
   void onGameResize(Vector2 size) {
-    print(size);
     super.onGameResize(size);
-    final testComponent = Test()
-      ..size = Vector2(size.x, size.y)
-      ..position = Vector2(0, 0);
-    world.add(testComponent);
-    print(world.componentsAtPoint(Vector2(0, 0)).length);
-    world.debugColor = Colors.amber;
-    camera.viewfinder.visibleGameSize = Vector2(size.x, size.y);
-    camera.viewfinder.position = Vector2(size.x / 2, 0);
-    camera.viewfinder.anchor = Anchor.topCenter;
+    final test = world.children.firstOrNull as Test?;
+    if (test != null) {
+      camera.viewfinder.position = Vector2(test.size.x / 2, 0);
+      camera.viewfinder.visibleGameSize = Vector2(size.x, size.y);
+    }
   }
 }

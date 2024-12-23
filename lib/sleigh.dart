9b9338c97c1b9613e1ac6b81f419f6ac50439223
sweetandsourkiss <mooul0529@naver.com>
2024-12-23 15:01:47 +0900

    Update main logic

--- lib/sleigh.dart
+++ lib/sleigh.dart
@@ -2,13 +2,39 @@ import 'dart:async';
 import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:spring/objects/chimney.dart';
+import 'package:spring/objects/obstacle.dart';
+import 'package:spring/objects/road.dart';
+import 'package:spring/objects/sleigh.dart';
+
+import './managers/map_manager.dart';
 
 class SleighGame extends FlameGame {
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    await images.loadAll([
+      'template_road.png',
+    ]);
     camera.viewfinder.anchor = Anchor.topCenter;
     camera.viewfinder.visibleGameSize = Vector2(size.x, size.y);
     camera.viewfinder.position = Vector2(size.x / 2, 0);
+    initializeGame();
+  }
+
+  void initializeGame() {
+    loadGameMap(0);
+  }
+
+  void loadGameMap(int mapIndex) {
+    for (final block in maps[mapIndex]) {
+      switch (block.blockType) {
+        case const (RoadBlock):
+          add(RoadBlock(gridPosition: block.gridPosition));
+        case const (ObstacleBlock):
+        case const (ChimneyBlock):
+        case const (SleighBlock):
+      }
+    }
   }
 
   @override

87341edc9474a22dae494ef2e3fd843e01de2bcd
sweetandsourkiss <mooul0529@naver.com>
2024-12-23 18:22:37 +0900

    Add offset

--- lib/objects/road.dart
+++ lib/objects/road.dart
@@ -5,15 +5,18 @@ import 'package:spring/sleigh.dart';
 
 class RoadBlock extends SpriteComponent with HasGameReference<SleighGame> {
   final Vector2 gridPosition;
+  final Vector2 offset;
 
   RoadBlock({
     required this.gridPosition,
+    required this.offset,
   }) : super(size: Vector2.all(64), anchor: Anchor.bottomLeft);
 
   @override
   FutureOr<void> onLoad() {
     final roadImage = game.images.fromCache('template_road.png');
     sprite = Sprite(roadImage);
-    position = Vector2(gridPosition.x * size.x, gridPosition.y * size.y);
+    position = Vector2(
+        gridPosition.x * size.x + offset.x, gridPosition.y * size.y + offset.y);
   }
 }

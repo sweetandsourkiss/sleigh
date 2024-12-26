093f3860cf5872547bc3cafedfcb6cfbd5cf6a10
sweetandsourkiss <mooul0529@naver.com>
2024-12-26 17:00:03 +0900

    Update load all

--- lib/sleigh.dart
+++ lib/sleigh.dart
@@ -6,14 +6,42 @@ import 'package:spring/objects/chimney.dart';
 import 'package:spring/objects/obstacle.dart';
 import 'package:spring/objects/road.dart';
 import 'package:spring/objects/sleigh.dart';
-
 import './managers/map_manager.dart';
 
 class SleighGame extends FlameGame {
   @override
   FutureOr<void> onLoad() async {
     await images.loadAll([
-      'road_intersection.png',
+      'road/road_intersection.png',
+      'road/road_1.png',
+      'road/road_2.png',
+      'road/road_3.png',
+      'road/road_4.png',
+      'road/road_12.png',
+      'road/road_13.png',
+      'road/road_14.png',
+      'road/road_23.png',
+      'road/road_24.png',
+      'road/road_34.png',
+      'road/road_234.png',
+      'road/road_123.png',
+      'road/road_124.png',
+      'road/road_134.png',
+      'chimney/chimney_intersection.png',
+      'chimney/chimney_1.png',
+      'chimney/chimney_2.png',
+      'chimney/chimney_3.png',
+      'chimney/chimney_4.png',
+      'chimney/chimney_12.png',
+      'chimney/chimney_13.png',
+      'chimney/chimney_14.png',
+      'chimney/chimney_23.png',
+      'chimney/chimney_24.png',
+      'chimney/chimney_34.png',
+      'chimney/chimney_234.png',
+      'chimney/chimney_123.png',
+      'chimney/chimney_124.png',
+      'chimney/chimney_134.png',
     ]);
     camera.viewfinder.anchor = Anchor.topCenter;
     camera.viewfinder.visibleGameSize = Vector2(size.x, size.y);
@@ -36,10 +64,21 @@ class SleighGame extends FlameGame {
                 size.x / 2 - maps[mapIndex].size / 2 * 64,
                 size.y / 2 - maps[mapIndex].size / 2 * 64,
               ),
+              ways: block.ways,
             ),
           );
         case const (ObstacleBlock):
         case const (ChimneyBlock):
+          add(
+            ChimneyBlock(
+              gridPosition: block.gridPosition,
+              offset: Vector2(
+                size.x / 2 - maps[mapIndex].size / 2 * 64,
+                size.y / 2 - maps[mapIndex].size / 2 * 64,
+              ),
+              ways: block.ways,
+            ),
+          );
         case const (SleighBlock):
       }
     }

83eada45aa41b97ee8a439784cd80cf47638966e
sweetandsourkiss <mooul0529@naver.com>
2024-12-26 17:00:25 +0900

    Add ways field

--- lib/managers/map_manager.dart
+++ lib/managers/map_manager.dart
@@ -7,8 +7,12 @@ import 'package:spring/objects/road.dart';
 class Block {
   final Vector2 gridPosition;
   final Type blockType;
+  final String ways;
 
-  Block({required this.gridPosition, required this.blockType});
+  Block(
+      {required this.gridPosition,
+      required this.blockType,
+      required this.ways});
 }
 
 final maps = [
@@ -16,21 +20,25 @@ final maps = [
 ];
 
 final map1 = SleighStage(size: 4, blocks: [
-  Block(gridPosition: Vector2(0, 0), blockType: RoadBlock),
-  Block(gridPosition: Vector2(1, 0), blockType: RoadBlock),
-  Block(gridPosition: Vector2(2, 0), blockType: RoadBlock),
-  Block(gridPosition: Vector2(3, 0), blockType: ChimneyBlock),
-  Block(gridPosition: Vector2(0, 1), blockType: RoadBlock),
-  Block(gridPosition: Vector2(1, 1), blockType: RoadBlock),
-  Block(gridPosition: Vector2(2, 1), blockType: RoadBlock),
-  Block(gridPosition: Vector2(3, 1), blockType: RoadBlock),
-  Block(gridPosition: Vector2(0, 2), blockType: RoadBlock),
-  Block(gridPosition: Vector2(1, 2), blockType: RoadBlock),
-  Block(gridPosition: Vector2(2, 2), blockType: RoadBlock),
-  Block(gridPosition: Vector2(3, 2), blockType: RoadBlock),
-  Block(gridPosition: Vector2(0, 3), blockType: RoadBlock),
-  Block(gridPosition: Vector2(0, 3), blockType: SleighBlock),
-  Block(gridPosition: Vector2(1, 3), blockType: RoadBlock),
-  Block(gridPosition: Vector2(2, 3), blockType: RoadBlock),
-  Block(gridPosition: Vector2(3, 3), blockType: RoadBlock),
+  Block(gridPosition: Vector2(0, 0), blockType: RoadBlock, ways: "23"),
+  Block(gridPosition: Vector2(1, 0), blockType: RoadBlock, ways: "234"),
+  Block(gridPosition: Vector2(2, 0), blockType: RoadBlock, ways: "234"),
+  Block(gridPosition: Vector2(3, 0), blockType: ChimneyBlock, ways: "34"),
+  Block(gridPosition: Vector2(0, 1), blockType: RoadBlock, ways: "123"),
+  Block(
+      gridPosition: Vector2(1, 1), blockType: RoadBlock, ways: "intersection"),
+  Block(
+      gridPosition: Vector2(2, 1), blockType: RoadBlock, ways: "intersection"),
+  Block(gridPosition: Vector2(3, 1), blockType: RoadBlock, ways: "134"),
+  Block(gridPosition: Vector2(0, 2), blockType: RoadBlock, ways: "123"),
+  Block(
+      gridPosition: Vector2(1, 2), blockType: RoadBlock, ways: "intersection"),
+  Block(
+      gridPosition: Vector2(2, 2), blockType: RoadBlock, ways: "intersection"),
+  Block(gridPosition: Vector2(3, 2), blockType: RoadBlock, ways: "134"),
+  Block(gridPosition: Vector2(0, 3), blockType: RoadBlock, ways: "12"),
+  Block(gridPosition: Vector2(0, 3), blockType: SleighBlock, ways: "124"),
+  Block(gridPosition: Vector2(1, 3), blockType: RoadBlock, ways: "124"),
+  Block(gridPosition: Vector2(2, 3), blockType: RoadBlock, ways: "124"),
+  Block(gridPosition: Vector2(3, 3), blockType: RoadBlock, ways: "14"),
 ]);
--- lib/objects/road.dart
+++ lib/objects/road.dart
@@ -6,15 +6,17 @@ import 'package:spring/sleigh.dart';
 class RoadBlock extends SpriteComponent with HasGameReference<SleighGame> {
   final Vector2 gridPosition;
   final Vector2 offset;
+  final String ways;
 
   RoadBlock({
     required this.gridPosition,
     required this.offset,
+    required this.ways,
   }) : super(size: Vector2.all(64), anchor: Anchor.topLeft);
 
   @override
   FutureOr<void> onLoad() {
-    final roadImage = game.images.fromCache('road_intersection.png');
+    final roadImage = game.images.fromCache('road/road_$ways.png');
     sprite = Sprite(roadImage);
     position = Vector2(
         gridPosition.x * size.x + offset.x, gridPosition.y * size.y + offset.y);
